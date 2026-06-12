7d58f2e2443af850324f72b52ba0d41d9061b8cd
Unnati Sharma <unnatisharma027@gmail.com>
2026-06-12 21:43:46 +0530

    Add files via upload

---
+++ trappingrainwater.java
@@ -0,0 +1,27 @@
+public class trappingrainwater {
+    public static int trap_max(int height[]){
+        int n=height.length;
+        int leftmax[]=new int[n];
+        leftmax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(height[i],leftmax[i-1]);
+        }
+        int rightmax[]=new int[n];
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
+        }
+        int trapped=0;
+        for(int i=0;i<n;i++){
+            int wl=Math.min(leftmax[i],rightmax[i]);
+            trapped +=wl-height[i];
+        }
+        return trapped;
+    }
+    public static void main(String args[]){
+        int arr[]={4,2,0,6,3,2,5};
+        int p=trap_max(arr);
+        System.out.print(p);
+    }
+    
+}
