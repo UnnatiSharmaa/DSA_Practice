746359936c1b470e8efbe755209d057377d098d4
Unnati Sharma <unnatisharma027@gmail.com>
2026-07-22 10:38:53 +0530

    Time: 2 ms (83.69%) | Memory: 47.7 MB (14.97%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        if (n < 3) return false;
+
+        int i = 0;
+
+        // walk up (strictly increasing)
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        // peak can't be first or last
+        if (i == 0 || i == n - 1) return false;
+
+        // walk down (strictly decreasing)
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+}
